31c00d1f396ef2122af29e01b25096b2393fbddc
Askar <a.suankulov@reply.com>
2024-03-08 12:28:17 +0000

    Initial commit

---
+++ core/src/main/java/com/xchange/core/workflows/GenerateImageWF.java
@@ -0,0 +1,23 @@
+package com.xchange.core.workflows;
+import com.adobe.granite.workflow.exec.WorkflowProcess;
+import com.day.cq.wcm.api.PageManager;
+import com.day.cq.wcm.api.WCMException;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import static org.osgi.service.component.annotations.ReferenceCardinality.OPTIONAL;
+
+
+@Slf4j
+@Component(service = WorkflowProcess.class, property = {"process.label=Generate Image via DALL-E"})
+public class GenerateImageWF implements WorkflowProcess {
+}
